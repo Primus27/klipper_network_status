c9f450588a7593e7dbc0baf52d32be8f5615d07a
Primus27 <47760587+Primus27@users.noreply.github.com>
2026-06-01 01:42:30 +0100

    feat: Initial rework and run network refresh off the reactor thread (#1)
    * chore: add gitignore
    
    * feat: rework fragile vars to dataclass
    
    * feat: put blocking calls on separate thread
    
    * feat: add actions workflow
    
    * chore: add PR template
    
    * docs: update README.md
    
    * fix: remove redundant formatting
    
    * chore: ruff format

---
+++ .github/pull_request_template.md
@@ -0,0 +1,21 @@
+## Summary
+
+<!-- What does this PR do and why? -->
+
+## Changes
+
+<!-- List the key changes made -->
+
+## Type of change
+
+- [ ] Bug fix
+- [ ] New feature
+- [ ] Refactor / cleanup
+- [ ] Documentation
+- [ ] CI / tooling
+
+## Checklist
+
+- [ ] `install.sh` updated if new files or dependencies were added
+- [ ] README updated if behaviour or configuration changed
+- [ ] No breaking changes to existing macro variables (or documented if so)
--- network_status.py
+++ network_status.py
@@ -1,37 +1,110 @@
-# Original: https://github.com/JeremyRuhland/klipper_network_status
-import os, logging
+import os
+import logging
+from dataclasses import dataclass, asdict
+from typing import Literal
+import socket
+import threading
+
+
+@dataclass
+class NetworkDetails:
+    status: Literal["Connected", "Disconnected", "N/A"] = "N/A"
+    ethip: str = "N/A"
+    wifiip: str = "N/A"
+    wifissid: str = "N/A"
+    mdns: str = "N/A"
+
 
 class network_status:
     def __init__(self, config):
-        self.interval = config.getint('interval', 60, minval=10)
-        self.ethip = "N/A"
-        self.wifiip = "N/A"
-        self.wifissid = "N/A"
-        self.mdns = "N/A"
-        self.last_eventtime = 0
+        """
+        Init
 
-    def get_status(self, eventtime):
-        if eventtime - self.last_eventtime > self.interval:
-            self.last_eventtime = eventtime
-            logging.info("network_status get_status %d" % eventtime)
-            try:
-                self.ethip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
-            except:
-                self.ethip = "N/A"
+        :param config: config object from Klipper
+        """
+        self.printer = config.get_printer()
+        self.interval_secs = config.getint("interval", 60, minval=10)
+        self.details = NetworkDetails()
 
-            try:
-                self.wifiip = os.popen('ip addr show wlan0').read().split("inet ")[1].split("/")[0]
-                self.wifissid = os.popen('iwgetid -r').read()[:-1]
-            except:
-                self.wifiip = "N/A"
-                self.wifissid = "N/A"
+        self.lock = threading.Lock()
+        self.stop_event = threading.Event()
+        self.refresh_thread = None
+        self.printer.register_event_handler("klippy:ready", self._handle_ready)
+        self.printer.register_event_handler(
+            "klippy:disconnect", self._handle_disconnect
+        )
 
-            self.mdns = os.popen('hostname').read()[:-1] + '.local'
+    def _handle_ready(self):
+        """
+        Refresh network info on a background thread so the blocking subprocess calls never stall Klipper's main thread
+        """
+        self.stop_event.clear()
+        self.refresh_thread = threading.Thread(
+            target=self._refresh_loop, name="network_status"
+        )
+        self.refresh_thread.daemon = True
+        self.refresh_thread.start()
 
-        return {'ethip': self.ethip,
-            'wifiip': self.wifiip,
-            'wifissid': self.wifissid,
-            'mdns': self.mdns}
+    def _handle_disconnect(self):
+        """
+        Set stop event in case of disconnection to exit refresh loop early
+        """
+        self.stop_event.set()
+
+    def _refresh_loop(self):
+        """
+        Update status loop caller
+        """
+        while not self.stop_event.is_set():
+            self._refresh()
+            # Wait returns early if stop_event is set, so shutdown is prompt.
+            self.stop_event.wait(self.interval_secs)
+
+    def _refresh(self):
+        """
+        Method to update network details
+        """
+        logging.debug("network_status refresh")
+        details = NetworkDetails()
+
+        try:
+            details.ethip = (
+                os.popen("ip addr show eth0").read().split("inet ")[1].split("/")[0]
+            )
+        except Exception:
+            pass
+
+        try:
+            details.wifiip = (
+                os.popen("ip addr show wlan0").read().split("inet ")[1].split("/")[0]
+            )
+            details.wifissid = os.popen("iwgetid -r").read()[:-1]
+        except Exception:
+            pass
+
+        try:
+            details.mdns = socket.gethostname() + ".local"
+        except Exception:
+            pass
+
+        details.status = (
+            "Connected"
+            if details.ethip != "N/A" or details.wifiip != "N/A"
+            else "Disconnected"
+        )
+
+        with self.lock:
+            self.details = details
+
+    def get_status(self, eventtime) -> dict:
+        """
+        Klipper call method
+
+        :return: Network details
+        """
+        with self.lock:
+            return asdict(self.details)
+
 
 def load_config(config):
     return network_status(config)
