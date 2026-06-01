156211616b372d3b15be0f9f6e01a51352bf5c77
Primus27 <47760587+Primus27@users.noreply.github.com>
2026-06-01 01:25:56 +0100

    chore: ruff format

--- network_status.py
+++ network_status.py
@@ -5,6 +5,7 @@ from typing import Literal
 import socket
 import threading
 
+
 @dataclass
 class NetworkDetails:
     status: Literal["Connected", "Disconnected", "N/A"] = "N/A"
@@ -22,14 +23,16 @@ class network_status:
         :param config: config object from Klipper
         """
         self.printer = config.get_printer()
-        self.interval_secs = config.getint('interval', 60, minval=10)
+        self.interval_secs = config.getint("interval", 60, minval=10)
         self.details = NetworkDetails()
-        
+
         self.lock = threading.Lock()
         self.stop_event = threading.Event()
         self.refresh_thread = None
         self.printer.register_event_handler("klippy:ready", self._handle_ready)
-        self.printer.register_event_handler("klippy:disconnect", self._handle_disconnect)
+        self.printer.register_event_handler(
+            "klippy:disconnect", self._handle_disconnect
+        )
 
     def _handle_ready(self):
         """
@@ -37,18 +40,17 @@ class network_status:
         """
         self.stop_event.clear()
         self.refresh_thread = threading.Thread(
-            target=self._refresh_loop,
-            name="network_status"
+            target=self._refresh_loop, name="network_status"
         )
         self.refresh_thread.daemon = True
         self.refresh_thread.start()
-    
+
     def _handle_disconnect(self):
         """
         Set stop event in case of disconnection to exit refresh loop early
         """
         self.stop_event.set()
-    
+
     def _refresh_loop(self):
         """
         Update status loop caller
@@ -57,7 +59,7 @@ class network_status:
             self._refresh()
             # Wait returns early if stop_event is set, so shutdown is prompt.
             self.stop_event.wait(self.interval_secs)
-    
+
     def _refresh(self):
         """
         Method to update network details
@@ -66,22 +68,30 @@ class network_status:
         details = NetworkDetails()
 
         try:
-            details.ethip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
+            details.ethip = (
+                os.popen("ip addr show eth0").read().split("inet ")[1].split("/")[0]
+            )
         except Exception:
             pass
 
         try:
-            details.wifiip = os.popen('ip addr show wlan0').read().split("inet ")[1].split("/")[0]
-            details.wifissid = os.popen('iwgetid -r').read()[:-1]
+            details.wifiip = (
+                os.popen("ip addr show wlan0").read().split("inet ")[1].split("/")[0]
+            )
+            details.wifissid = os.popen("iwgetid -r").read()[:-1]
         except Exception:
             pass
 
         try:
-            details.mdns = socket.gethostname() + '.local'
+            details.mdns = socket.gethostname() + ".local"
         except Exception:
             pass
 
-        details.status = "Connected" if details.ethip != "N/A" or details.wifiip != "N/A" else "Disconnected"
+        details.status = (
+            "Connected"
+            if details.ethip != "N/A" or details.wifiip != "N/A"
+            else "Disconnected"
+        )
 
         with self.lock:
             self.details = details
@@ -94,6 +104,7 @@ class network_status:
         """
         with self.lock:
             return asdict(self.details)
+
 
 def load_config(config):
     return network_status(config)
