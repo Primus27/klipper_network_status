516e0dd5239d48a75911acf6ef5ebe21a7e8c338
Primus27 <47760587+Primus27@users.noreply.github.com>
2026-06-02 02:06:18 +0100

    feat: add warning if klipper install is non-standard

--- install.sh
+++ install.sh
@@ -4,14 +4,20 @@ set -e
 KLIPPER_PATH="${HOME}/klipper"
 SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ "$EUID" -eq 0 ]; then
+    echo "This script must not run as root"
+    exit 1
+fi
+
 while getopts "k:" arg; do
     case $arg in
         k) KLIPPER_PATH=$OPTARG;;
     esac
 done
 
-if [ "$EUID" -eq 0 ]; then
-    echo "This script must not run as root"
+if [ ! -d "${KLIPPER_PATH}/klippy/extras" ]; then
+    echo "Klipper extras directory not found at ${KLIPPER_PATH}/klippy/extras"
+    echo "Use -k to specify your Klipper path"
     exit 1
 fi
 
